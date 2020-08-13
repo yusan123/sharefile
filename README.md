5c8d2846e62638c604febf36a4c8d7f4dd176700
Administrator <823947488@qq.com>
2020-08-13 19:50:05 +0800

    格式调整准备上传github

---
+++ README.md
@@ -0,0 +1,3 @@
+### 可以通过本地磁盘的指定大小的空间，实现共享的网盘，可以上传下载，删除，打包下载，批量删除.
+
+###A shared network disk can be realized through the specified size of the local disk space, which can be uploaded and downloaded, deleted, packaged and downloaded, and batch deleted.
--- src/main/java/com/yu/MainApplicaion.java
+++ src/main/java/com/yu/MainApplicaion.java
@@ -10,11 +10,8 @@ public class MainApplicaion {
      * 指定磁盘目录共享文件，可以上传下载删除，批量删除，批量下载，导出excel
      * 可以指定上传文件大小，总空间大小，支持对中文或特殊字符文件名文件上传
      *
-     * 在linux上通过
-     * #!/bin/bash
-     * java -Dfile.encoding=utf-8 -jar sharefile-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+     * 在linux上通过     * java -Dfile.encoding=utf-8 -jar sharefile-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
      *
-     * tail -f sharefile.log
      * @param args
      */
     public static void main(String[] args) {
