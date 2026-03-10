f22317aaf84262d68fd87202eb1db3243184880e
yusen <yusen@xinchan.com.cn>
2026-03-10 10:27:11 +0800

    引入Sa-Token增加认证

--- src/main/java/com/yu/controller/FileController.java
+++ src/main/java/com/yu/controller/FileController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import sun.rmi.runtime.Log;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
